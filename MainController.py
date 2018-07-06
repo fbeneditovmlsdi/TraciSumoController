968dfe9b4af06e4fba3a8dcbb56fa369bb88aa67
fbeneditovm <fbeneditovm@lsdi.ufma.br>
2018-07-06 16:30:47 -0300

    [MainController] Trying to get late values for punctuality

--- MainController.py
+++ MainController.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import os
 import sys
 import optparse
+import math
 import subprocess
 import random
 import time
@@ -22,8 +23,13 @@ except ImportError:
 
 import traci
 
-t_type = {"PATH":0, "PUNCTUALITY":1, "SPEED_MOV":2, "SPEED_STILL":3}
+t_type = {"PATH":0, "PUNCTUALITY":1, "SPEED_MOV":2, "SPEED_STILL":3, "DISTANCE":4,}
 timestamp = {"NOW":long(round(time.time() * 1000)), "4:00":1530601200000, "8:00":1530615600000, "10:46":1530625560000}
+
+
+def distance(x1,y1, x2,y2):
+    dist = ((x1-x2)**2)+((y1-y2)**2)
+    return math.sqrt(dist)
 
 
 def run_simutaion(test_type = 0):
@@ -91,6 +97,30 @@ def run_simutaion(test_type = 0):
                                            + str(step) + "\n")
             step += 1
 
+    if test_type == t_type["DISTANCE"]:
+        logfile_name = "[DISTANCE]"+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"ConstBreakLog.txt"
+        with open(logfile_name, "w") as breakLog:
+            # write the current time to the log
+            breakLog.write(str(timestamp["8:00"])+"\n")
+        edges = ["327676501#0", "327676501#1", "327676501#2", "433033617#0", "433033617#1", "433033617#2",
+                 "433033617#3", "343294482#3", "343294482#4", "343294482#5", "327676481#3", "327676481#4", "327676512#0"]
+        car_set = set([])
+
+        while traci.simulation.getMinExpectedNumber() > 0:
+            traci.simulationStep()  # Run a simulation step
+            car_set.update(traci.edge.getLastStepVehicleIDs(edges[0])) # get the cars at the edge
+            car_li = list(car_set)
+            for i in range(0, len(list)):
+                x1, y1 = traci.vehicle.getPosition(car_li[i])
+                for j in range(i, len(list)):
+                    x2, y2 = traci.vehicle.getPosition(car_li[j])
+                    dist = distance(x1, y1, x2, y2)
+                    dist_string = "v_id1: " + str(car_li[i]) + ", v_id2: " + str(car_li[j]) + ", distance: " + str(dist)
+                    print(dist_string)
+                    with open(logfile_name, "a") as breakLog:
+                        breakLog.write(dist_string + "\n")
+            step += 1
+
     traci.close()
     sys.stdout.flush()
 
@@ -114,7 +144,7 @@ if __name__ == "__main__":
     else:
         sumoBinary = checkBinary('sumo-gui')
 
-    test_type = t_type["PUNCTUALITY"]
+    test_type = t_type["DISTANCE"]
 
     curr_datetime = datetime.datetime.now()
 
@@ -124,16 +154,14 @@ if __name__ == "__main__":
     # subprocess and then the python script connects and runs
     if test_type == t_type["PATH"]:
         traci.start([sumoBinary, "-c", "ufma.sumocfg"])
-    """
-    if test_type == t_type["PUNCTUALITY"]:
-        traci.start([sumoBinary, "-c", "ufma_punctuality.sumocfg"])
-    """
     if test_type == t_type["PUNCTUALITY"]:
         traci.start([sumoBinary, "-c", "ufma_punctuality.sumocfg"])
     if test_type == t_type["SPEED_MOV"]:
         traci.start([sumoBinary, "-c", "ufma_speed.sumocfg"])
     if test_type == t_type["SPEED_STILL"]:
         traci.start([sumoBinary, "-c", "ufma_not_still.sumocfg"])
+    if test_type == t_type["DISTANCE"]:
+        traci.start([sumoBinary, "-c", "ufma.sumocfg"])
     run_simutaion(test_type)
 
 """
